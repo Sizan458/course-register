0d1eaf7868e4f7fec870d5d1d5f142bf352e9cdc
Sizan458 <www.shijan23@gmail.com>
2023-09-14 23:12:19 +0600

    fatch cards

--- src/component/CardDiv.jsx
+++ src/component/CardDiv.jsx
@@ -8,13 +8,34 @@ const CardDiv = () => {
     useEffect(() =>{
         fetch('./course.json')
         .then(res=>res.json())
-        .then(data =>setData(data))
+        .then(card =>setData(card))
     },[] )
     return (
-        <div>
+        <div >
             <h1 className=" text-3xl text-center font-bold text-black mt-3">Course Registration</h1>
-            <div>
-                <h2>Course:{data.length}</h2>
+            
+            <div className="flex mt-5 w-[90%] mx-auto">
+               <div className="grid grid-cols-3  gap-3  ">
+               {
+                data.map(data=>(
+                    <div className="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+                )
+                    
+                )
+               }
+               </div>
+               <div>
+                <h3>Cart</h3>
+               </div>
             </div>
 
         </div>
