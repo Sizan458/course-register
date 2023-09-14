5e4db263baa4decbfb66e4e9da4621eff1a7bcbf
Sizan458 <www.shijan23@gmail.com>
2023-09-15 01:05:36 +0600

    fatch cards & design cards

--- src/component/CardDiv.jsx
+++ src/component/CardDiv.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import {FaDollarSign,FaBookOpen} from 'react-icons/fa';
 
 
 const CardDiv = () => {
@@ -14,17 +15,26 @@ const CardDiv = () => {
         <div >
             <h1 className=" text-3xl text-center font-bold text-black mt-3">Course Registration</h1>
             
-            <div className="flex mt-5 w-[90%] mx-auto">
-               <div className="grid grid-cols-3  gap-3  ">
+            <div className="flex mt-5  w-[90%] mx-auto">
+               <div className="grid grid-cols-3   gap-2 w-[80%]  text-black">
                {
                 data.map(data=>(
-                    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card bg-base-100 shadow-xl">
+  <figure><img src={data.image } className="w-full mt-4 p-3"/></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <h2 className="card-title text-black">{data.title}</h2>
+    <p className="text-base">{data.description}</p>
+    <div className="flex gap-10 mt-2">
+        <div >
+           <p>  <button className="text-xl "> <FaDollarSign></FaDollarSign> </button> <span>Price:{data.price}</span> </p>
+          
+        </div>
+        <div>
+         <p> <button className="text-xl gap-1"> <FaBookOpen></FaBookOpen> </button> Credit:{data.credit}hr</p>
+        </div>
+    </div>
+    <div className="card-actions ">
+      <button className="btn btn-success w-full mt-3 text-white font-bold text-xl">Select</button>
     </div>
   </div>
 </div>
