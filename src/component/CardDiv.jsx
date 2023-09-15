28136480f6c1946b451ef332be2fee1311450b8f
Sizan458 <www.shijan23@gmail.com>
2023-09-15 12:33:17 +0600

    add functions on cart section

--- src/component/CardDiv.jsx
+++ src/component/CardDiv.jsx
@@ -2,25 +2,56 @@ import { useEffect } from "react";
 import { useState } from "react";
 import {FaDollarSign,FaBookOpen} from 'react-icons/fa';
 import Cart from "./cart";
+import swal from 'sweetalert'
 
 
 const CardDiv = () => {
     const [data, setData] =  useState([])
+    const [selectCourse,setSelectCourse] = useState([])
+    const[price,setPrice] = useState(0)
+    const[credit,setCredit]=useState(0)
+    const[RemainingCredit,setRemainingCredit]=useState(0)
 
     useEffect(() =>{
         fetch('./course.json')
         .then(res=>res.json())
         .then(card =>setData(card))
     },[] )
+const handleCourse=(data) =>{
+    const isExit=selectCourse. find (item=>item.id===data.id)
+    let price=data.price;
+    let Credit=data.credit;
+    if(isExit){
+        return swal("Already selected this course");
+    }else{
+        selectCourse.forEach(dollar=>{
+           price+=dollar.price
+        });
+       setPrice(price);
+      
+       selectCourse.forEach(credit=>{
+           Credit+=credit.credit
+       })
+       const RemainingCredit= 20-Credit
+       setRemainingCredit(RemainingCredit)
+       setCredit(Credit)
+        setSelectCourse([...selectCourse,data])
+    }
+    
+   
+    
+    
+}
+
     return (
-        <div >
+        <div className="mb-2">
             <h1 className=" text-3xl text-center font-bold text-black mt-3">Course Registration</h1>
             
-            <div className="flex mt-5  w-[90%] mx-auto gap-4">
-               <div className="grid grid-cols-3   gap-2 w-[80%]  text-black">
+            <div className="flex w-[90%] mx-auto gap-4">
+               <div className="grid grid-cols-3 gap-3 w-[80%]">
                {
                 data.map(data=>(
-                    <div className="card bg-base-100 shadow-xl">
+                    <div className="card bg-base-100 shadow-xl"  key={data.id}>
   <figure><img src={data.image } className="w-full mt-4 p-3"/></figure>
   <div className="card-body">
     <h2 className="card-title text-black">{data.title}</h2>
@@ -35,7 +66,7 @@ const CardDiv = () => {
         </div>
     </div>
     <div className="card-actions ">
-      <button className="btn btn-success w-full mt-3 text-white font-bold text-xl">Select</button>
+      <button className="btn btn-success w-full mt-3 text-white font-bold text-xl" onClick={()=>handleCourse(data)}>Select</button>
     </div>
   </div>
 </div>
@@ -45,7 +76,7 @@ const CardDiv = () => {
                }
                </div>
                <div >
-                <Cart></Cart>
+                <Cart selectCourse={selectCourse} price={price} credit={credit} RemainingCredit={RemainingCredit} ></Cart>
                </div>
             </div>
 
--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,22 +1,36 @@
-
-const Cart = () => {
+import PropTypes from 'prop-types'
+const Cart = ({selectCourse,price,credit,RemainingCredit}) => {
+    console.log(selectCourse)
     return (
         <div className="mt-6 p-4 border-2 border-green-400">
-            <h2 className="  text-xl  font-bold text-sky-500  ">Credit Hour Remaining</h2>
+            <h2 className="    font-bold text-sky-500  ">Credit Hour Remaining {RemainingCredit } hr</h2>
             <hr  className="mt-4"/>
             <div>
                 <h2 className="text-center mt-3 text-xl font-bold"> Course Name</h2>
+                <div className='mt-3'>
+                  {
+                    selectCourse.map(courses=>(
+                        <li key={courses.id}className='list-decimal'>{courses.title }</li>
+                       
+                    ))
+                  }
+                </div>
             </div>
             <hr className="mt-5"/>
             <div>
-                <h2 className="text-xl mt-2 font-bold">Total Credit Hour :</h2>
+                <h2 className="text-xl mt-2 font-bold">Total Credit Hour :{credit}</h2>
             </div>
             <hr className="mt-3"/>
             <div>
-                <h2 className="mt-4 text-xl font-bold" >Total Price :</h2>
+                <h2 className="mt-4 text-xl font-bold" >Total Price :{price}$</h2>
             </div>
         </div>
     );
 };
-
+Cart.propTypes={
+    selectCourse: PropTypes.array,
+    price: PropTypes.number,
+    credit: PropTypes.number,
+    RemainingCredit: PropTypes.number
+}
 export default Cart;
